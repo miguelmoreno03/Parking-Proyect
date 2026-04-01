9b84431fa5aeafbab75a76fd5f9827de1e8a8bfc
Miguel Moreno <miguelangelmorenomogollon@gmail.com>
2026-04-01 18:42:02 -0500

    Ading exists by type in the rate repository

--- Back End/parking-system/src/main/java/com/_bit/solutions/parking_system/repository/RateRepository.java
+++ Back End/parking-system/src/main/java/com/_bit/solutions/parking_system/repository/RateRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 @Repository
 public interface RateRepository extends JpaRepository <Rate,Long> {
-
     Optional<Rate> findByType(Type type);
+    boolean existingByType(Type type);
 }
